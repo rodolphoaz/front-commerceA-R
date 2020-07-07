3c68da30248e034c84d1c56f9769f1a96e75f3fd
Rodolphoaz <rod-azeredo@hotmail.com>
2020-07-06 23:08:38 -0300

    commit initial

---
+++ somar.js
@@ -0,0 +1,11 @@
+
+
+
+
+ function clicar() {
+
+    var num2 = document.querySelector(".num2").value;
+
+    var resultado = (num2*140); 
+    document.querySelector(".resultado").innerHTML = resultado;
+}
