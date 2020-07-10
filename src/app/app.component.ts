2a2a2924eeb3edfa3d0815a9e2c9557000e6af16
DosSantosCarlos <anjoazrael10@gmail.com>
2020-07-10 19:09:30 -0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerce-angular';
+  name = '';
+
+  constructor( private route: ActivatedRoute) {
+    
+  }
+
+  ngOnInit(){
+    this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
+      console.log(this.name)
+    });
+  }
+
 }
