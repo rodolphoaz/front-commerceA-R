203c6076763c6ab724a5bc3d6ec4688403cdfccd
DosSantosCarlos <anjoazrael10@gmail.com>
2020-08-01 09:09:34 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -17,48 +17,50 @@ Auth::routes();
 
 Route::get('/', function (){
 	return redirect()->route('home');
-	return redirect()->route('individual.index');
 	
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(function () {
+	Route::get('product', 'ProductController@index')->name('product.index');
 
-Route::get('/email','EmailController@index')->name('email');
+	Route::get('product/{category}/{subcategory?}','ProductController@show')
+				->where(['category'=> '[a-z]+' , 'subcategory' => '[a-z]+'])
+				->name('product.show');
+			
+	Route::get('email','EmailController@index')->name('email');
 
-Route::get('support','SupportController@index')->name('suport.index');
+	Route::get('home', 'HomeController@index')->name('home');
 
-Route::get('/contact', 'ContactController@index')->name('contact');
-Route::post('/contact' , 'ContactController@store')->name('contact.store');
+	Route::get('support','SupportController@index')->name('suport.index');
 
-Route::get('/about', 'AboutController@index')->name('about.index');
+	Route::get('contact', 'ContactController@index')->name('contact');
+	Route::post('contact' , 'ContactController@store')->name('contact.store');
 
-/*Mugs*/ 
-
-/*digital*/ 
-
-/*a lapis*/ 
-
-/*quadros*/ 
-
-/*Mascotes*/ 
+	Route::get('about', 'AboutController@index')->name('about.index');
+});
 
 Route::prefix('UAXEE112')->middleware(['auth'])->group( function(){	
+	
 	Route::get('funcionario','EmployeeController@index')->name('employee.index');
-	Route::resource('product', 'ProductController')->name('product.resource');
+	
 	Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+	
 	Route::put('profile/password', 'ProfileController@password')->name('profile.password');
 	Route::get('profile', 'ProfileController@edit')->name('profile.edit');
 	Route::put('profile', 'ProfileController@update')->name('profile.update');
 });
 
 Route::prefix('cliente')->middleware(['auth'])->group( function () {
+	
 	Route::get('profile/{id}', 'ProfileController@edit')->name('profile.edit');
 	Route::put('profile/{id}', 'ProfileController@update')->name('profile.update');
+	
 	Route::get('checkout', 'CheckoutController@index')->name('checkout.index');
 	Route::get('carrinho', 'Cartcontroller@index')->name('cart.index');
-
+	
+	Route::get('favoritos' ,  'ProductFavorite@index')->name('favorite.index');
+	Route::post('favorito',  'ProductFavorite@delete')->name('favorite.delete');
+	
+	Route::get('amei' ,  'ProductLike@index')->name('like.index');
+	Route::post('amei',  'ProductLike@delete')->name('like.delete');
 });
-
-Route::get('/product','ProductController@index')->name('product');
-
-Route::get('/email','EmailController@index')->name('email');
