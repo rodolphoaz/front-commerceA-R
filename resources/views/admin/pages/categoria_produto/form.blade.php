86e51b55a6f07f444d868073510180b2f208473d
rodolphoaz <rod-azeredo@hotmail.com>
2020-10-06 19:10:03 -0300

    pages para corrigir

---
+++ resources/views/admin/pages/categoria_produto/form.blade.php
@@ -0,0 +1,20 @@
+{!! Form::open()->route('category.'.$type)->fill($model)->autoComplete(false) !!}
+    @if($type=='update') 
+        {!! Form::hidden('id',null) !!}
+    @endif
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            {!! Form::text('sort','Tipo')->wrapperAttrs(['class' => 'col-12']) !!}
+            {!! Form::text('template' , 'Modelo')->wrapperAttrs(['class' => 'col-12']) !!}
+            {!! Form::text('price', 'Preco')->wrapperAttrs(['class' => 'col-12']) !!} 
+        </div>
+        <div class="col-6">
+            {!! Form::file('upload','Selecione as fotos')->wrapperAttrs(['class'=> 'col-12']) !!}
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        {!! Form::submit("Salvar")->attrs(['class' => 'btn btn-primary col-1']) !!}
+    </div>
+</div>
+{!! Form::close() !!}
---
+++ resources/views/admin/pages/partials/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.layouts.app', ['title' => 'create conta_user'])
+
+@section('content')
+
+    @include('admin.pages.partials.header', [
+        'title' => __('Criar ') . ' '. 'Nova Caricatura',
+        'description' => __('This is your profile page. You can see the progress you\'ve 
+        made with your work and manage your projects or assigned tasks'),
+        'class' => 'col-lg-7'
+    ]) 
+    
+    <div class="container mt-4 text-center">
+   
+        <h1 class="title text-center col-12">{{__('Nova Caricatura')}}</h1>
+        {!! Form::open()->route('caricatura.store')->autoComplete(false) !!}
+            <div class="row d-flex justify-content-center" >
+                {!! Form::text('tipo','Tipo')->wrapperAttrs(['class' => 'col-4']) !!}
+                {!! Form::text('modelo' , 'Modelo')->wrapperAttrs(['class' => 'col-4']) !!}
+                {!! Form::text('valor', 'valor')->wrapperAttrs(['class' => 'col-8']) !!}
+            </div>
+            <div class="row justify-content-center">
+                {!! Form::submit("Salvar")->attrs(['class' => 'btn btn-primary col-1']) !!}
+            </div>
+        {!! Form::close() !!}
+        </div>
+    </div>
+
+@endsection
